c994d54fbfa634fc3fe5181672e39fbe69dd771c
Dhavalsinh Prafulsinh Zala <dhavalsinh@cronsystems.com>
2018-11-09 13:43:45 +0530

    Updates

---
+++ precache-manifest.1e0662a3ab753f4ca1fb12aea12429e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "059fb1c122fd950a4be7",
+    "url": "/line-chart/static/js/runtime~main.059fb1c1.js"
+  },
+  {
+    "revision": "f943d13b3c16c91a3410",
+    "url": "/line-chart/static/js/main.f943d13b.chunk.js"
+  },
+  {
+    "revision": "35768086ca398f61437a",
+    "url": "/line-chart/static/js/1.35768086.chunk.js"
+  },
+  {
+    "revision": "f943d13b3c16c91a3410",
+    "url": "/line-chart/static/css/main.f4fb1b8f.chunk.css"
+  },
+  {
+    "revision": "35768086ca398f61437a",
+    "url": "/line-chart/static/css/1.3a90a7e2.chunk.css"
+  },
+  {
+    "revision": "280287dd3b0d1bb59bab850b691100c8",
+    "url": "/line-chart/index.html"
+  }
+];
